9ef4eb49082f6f0bce92f269ee0060465e8d5c0f
2dam3 <2dam3@HZ114488.H012108.NET>
2020-12-21 13:49:24 +0100

    añadidos las rutas del usuario

--- src/main/java/com/example/demo/controller/UsuarioController.java
+++ src/main/java/com/example/demo/controller/UsuarioController.java
@@ -1,11 +1,14 @@
 package com.example.demo.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.model.Usuarios;
 import com.example.demo.repository.UsuarioRepository;
@@ -21,9 +24,102 @@ public class UsuarioController {
 	@Autowired
 	private MongoTemplate mongoTemplate;
 
-	@PostMapping("/nuevo")
-	public void insertarDepartamento(@RequestBody Usuarios nuevoUsuario) {
+	/**************************************************/
+	/******************** INSERTAR ********************/
+	/**************************************************/
+
+	@PostMapping("/new")
+	public void insertarUsuario(@RequestBody Usuarios nuevoUsuario) {
 		usuarioRepository.save(nuevoUsuario);
 	}
 
+	/**************************************************/
+	/********************** LEER **********************/
+	/**************************************************/
+
+	@GetMapping("/all")
+	public List<Usuarios> getUsuarios() {
+		List<Usuarios> listaUsuarios = usuarioRepository.findAll();
+		return listaUsuarios;
+	}
+
+	@GetMapping("/getById/{id}")
+	public Optional<Usuarios> getById(@PathVariable String id){
+
+		return usuarioRepository.findById(id);
+	}
+
+	//lo mismo que el getById, pero en vez de un optional, devuelve un list
+	@GetMapping("/getAllById/{id}")
+	public List<Usuarios> getAllById(@PathVariable String id) {
+		return usuarioRepository.findUsuarioById(id);
+	}
+
+	@GetMapping("/getByUsuario/{usuario}")
+	public List<Usuarios> getUsuariosByUsuario(@PathVariable String usuario) {
+		return usuarioRepository.findByUsuario(usuario);
+	}
+
+
+	/**************************************************/
+	/******************* MODIFICAR ********************/
+	/**************************************************/
+
+	@PutMapping("/updateContraseñaById/{id}/{contraseña}")
+	public void updateContraseñaById(@PathVariable String id, @PathVariable String contraseña){
+
+		Query query = new Query(Criteria.where("id").is(id));
+		Update update = new Update().set("contraseña", contraseña);
+		mongoTemplate.updateFirst(query, update, Usuarios.class);
+
+	}
+
+	//MIRAR COMO HACER EL ACTUALIZAR EL USUARIO ENTERO, ESTO COMO EJEMPLO DE IR COLUMNA POR COLUMNA
+	@PutMapping("/updateUsuarioById/{id}/{nombre}/{apellidos}/{avatar}")
+	public void updateUsuarioById(@PathVariable String id, @PathVariable String nombre, @PathVariable String apellidos, @PathVariable String avatar){
+
+		Query query = new Query(Criteria.where("id").is(id));
+		Update update = new Update().set("nombre", nombre);
+		update.set("apellidos", apellidos);
+		update.set("avatar", avatar);
+		mongoTemplate.updateFirst(query, update, Usuarios.class);
+
+	}
+
+
+	@PutMapping("/conectarUsuario/{id}")
+	public void conectarUsuario(@PathVariable String id){
+
+		Query query = new Query(Criteria.where("id").is(id));
+		Update update = new Update().set("conectado", true);
+		mongoTemplate.updateFirst(query, update, Usuarios.class);
+
+	}
+
+	@PutMapping("/desconectarUsuario/{id}")
+	public void desconectarUsuario(@PathVariable String id){
+
+		Query query = new Query(Criteria.where("id").is(id));
+		Update update = new Update().set("conectado", false);
+		mongoTemplate.updateFirst(query, update, Usuarios.class);
+
+	}
+
+
+	/**************************************************/
+	/********************* BORRAR *********************/
+	/**************************************************/
+
+	@DeleteMapping("/deleteById/{id}")
+	public void delete(@PathVariable String id) {
+		usuarioRepository.deleteById(id);
+	}
+
+	@DeleteMapping("/deleteByUsername/{username}")
+	public void deleteByUsername(@PathVariable String username) {
+		usuarioRepository.deleteByUsuario(username);
+	}
+
 }
+
+
--- src/main/java/com/example/demo/repository/UsuarioRepository.java
+++ src/main/java/com/example/demo/repository/UsuarioRepository.java
@@ -1,9 +1,18 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.example.demo.model.Usuarios;
 
 public interface UsuarioRepository  extends MongoRepository<Usuarios, String>{
+
+
+	List<Usuarios> findByUsuario(String usuario);
+
+	void deleteByUsuario(String usuario);
+
+	List<Usuarios> findUsuarioById(String id);
 
 }
