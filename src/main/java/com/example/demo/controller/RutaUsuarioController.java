18b45e624daa343638d17686192b0e6eccd6d05b
2dam3 <2dam3@HZ114488.H012108.NET>
2020-12-21 14:45:55 +0100

    cambios en rutaUsuarios, a corregir porque no funciona

--- src/main/java/com/example/demo/controller/RutaUsuarioController.java
+++ src/main/java/com/example/demo/controller/RutaUsuarioController.java
@@ -1,5 +1,129 @@
 package com.example.demo.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.model.RutaUsuario;
+import com.example.demo.repository.RutaUsuarioRepository;
+
+@RequestMapping("/rutaUsuario")
+@RestController
 public class RutaUsuarioController {
 
+	@Autowired
+	private RutaUsuarioRepository rutaUsuarioRepository;
+
+	@Autowired
+	private MongoTemplate mongoTemplate;
+
+	/**************************************************/
+	/******************** INSERTAR ********************/
+	/**************************************************/
+
+	@PostMapping("/new")
+	public void insertarUsuario(@RequestBody RutaUsuario nuevaRutaUsuario) {
+		rutaUsuarioRepository.save(nuevaRutaUsuario);
+	}
+
+	/**************************************************/
+	/********************** LEER **********************/
+	/**************************************************/
+
+	@GetMapping("/all")
+	public List<RutaUsuario> getRutaUsuario() {
+		List<RutaUsuario> listaRutasUsuarios = rutaUsuarioRepository.findAll();
+		return listaRutasUsuarios;
+	}
+
+	@GetMapping("/getById/{id}")
+	public Optional<RutaUsuario> getById(@PathVariable String id){
+		return rutaUsuarioRepository.findById(id);
+	}
+
+	//lo mismo que el getById, pero en vez de un optional, devuelve un list
+	@GetMapping("/getAllById/{id}")
+	public List<RutaUsuario> getAllById(@PathVariable String id) {
+		return rutaUsuarioRepository.findRutaUsuarioById(id);
+	}
+
+
+	/*Hace falta comprobar que funciona*/
+	@GetMapping("/getAllByRutaId/{id}")
+	public List<RutaUsuario> getAllByRutaId(@PathVariable String id) {
+		return rutaUsuarioRepository.findRutaUsuarioByRuta_idAndOrderByPuntuacion(id);
+	}
+
+	/**************************************************/
+	/******************* MODIFICAR ********************/
+	/**************************************************/
+
+	@PutMapping("/updateContraseñaById/{id}/{contraseña}")
+	public void updateContraseñaById(@PathVariable String id, @PathVariable String contraseña){
+
+		Query query = new Query(Criteria.where("id").is(id));
+		Update update = new Update().set("contraseña", contraseña);
+		mongoTemplate.updateFirst(query, update, RutaUsuario.class);
+
+	}
+
+	@PutMapping("/updatePuntuacion/{id}/{puntuacion}")
+	public void updatePuntuacion(@PathVariable String id, @PathVariable int puntuacion) {
+
+		Query query = new Query(Criteria.where("id").is(id));
+		Update update = new Update().inc("puntuacion", puntuacion);
+		mongoTemplate.updateFirst(query, update, RutaUsuario.class);
+
+	 }
+
+
+	@PutMapping("/rutaUsuarioActivar/{id}")
+	public void conectarUsuario(@PathVariable String id){
+
+		Query query = new Query(Criteria.where("id").is(id));
+		Update update = new Update().set("activo", true);
+		mongoTemplate.updateFirst(query, update, RutaUsuario.class);
+
+	}
+
+	@PutMapping("/rutaUsuarioDesactivar/{id}")
+	public void desconectarUsuario(@PathVariable String id){
+
+		Query query = new Query(Criteria.where("id").is(id));
+		Update update = new Update().set("activo", false);
+		mongoTemplate.updateFirst(query, update, RutaUsuario.class);
+
+	}
+
+
+	/**************************************************/
+	/********************* BORRAR *********************/
+	/**************************************************/
+
+	@DeleteMapping("/deleteById/{id}")
+	public void delete(@PathVariable String id) {
+		rutaUsuarioRepository.deleteById(id);
+	}
+
+	@DeleteMapping("/deleteByUsuarioId/{id}")
+	public void deleteByUsuarioId(@PathVariable String id) {
+		rutaUsuarioRepository.deleteByUsuario_id(id);
+	}
+
+	@DeleteMapping("/deleteByRutaId/{id}")
+	public void deleteByRutaId(@PathVariable String id) {
+		rutaUsuarioRepository.deleteByRuta_id(id);
+	}
+
+	//si te cansas de la vida y quieres borrar todo
+	@DeleteMapping("/deleteAll")
+	public void deleteUsuarios(){
+		rutaUsuarioRepository.deleteAll();
+	}
 }
--- src/main/java/com/example/demo/repository/RutaUsuarioRepository.java
+++ src/main/java/com/example/demo/repository/RutaUsuarioRepository.java
@@ -1,9 +1,19 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.example.demo.model.RutaUsuario;
 
 public interface RutaUsuarioRepository  extends MongoRepository<RutaUsuario, String>{
+
+	void deleteByUsuario_id(String id);
+
+	void deleteByRuta_id(String id);
+
+	List<RutaUsuario> findRutaUsuarioById(String id);
+
+	List<RutaUsuario> findRutaUsuarioByRuta_idAndOrderByPuntuacion(String id);
 
 }
